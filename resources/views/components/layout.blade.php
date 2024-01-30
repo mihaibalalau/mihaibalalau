9c9667971769b3cd6dd4c9b9cdc3eaa1b99cbb1f
Mihai Balalau <mihaibalalau@gmail.com>
2024-01-30 05:03:34 +0200

    added fonts

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,11 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mihai Balalau</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
 </head>
 
 <body>
-    {{$slot}}
-    <script type="application/javacsript" src="/app.js"></script>
+    {{ $slot }}
 </body>
 
 </html>
