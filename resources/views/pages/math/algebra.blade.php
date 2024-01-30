caf09bedfe5e0bd730c42b670523d5ae38b412f8
Mihai Balalau <mihaibalalau@gmail.com>
2024-01-30 07:07:46 +0200

    added KaTeX

--- resources/views/pages/math/algebra.blade.php
+++ resources/views/pages/math/algebra.blade.php
@@ -1,13 +1,17 @@
 <x-layout>
     <x-slot:head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.13.0/katex.min.css"
-            integrity="sha384-R4558gJ3+9R5D1A+Hw3PDXCG+QG4Swhp0vYhIMpK8ENSOwEnClIDZZAM+SBh8Q1F" crossorigin="anonymous">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.13.0/katex.min.js"
-            integrity="sha384-fWURyB/6aPcIcKjRTPVFbETlFtRqNgp2krv4pJ5f6kPtJ8KDxK2e5IvRbUR7GxBI" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css"
+            integrity="sha384-n8MVd4RsNIU0tAv4ct0nTaAbDJwPJzDEaqSD1odI+WdtXRGWt2kTvGFasHpSy3SV" crossorigin="anonymous">
+
+        <!-- The loading of KaTeX is deferred to speed up page rendering -->
+        <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.js"
+            integrity="sha384-XjKyOOlGwcjNTAIQHIpgOno0Hl1YQqzUOEleOLALmuqehneUG+vnGctmUb0ZY0l8" crossorigin="anonymous">
         </script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.13.0/contrib/auto-render.min.js"
-            integrity="sha384-Um5gpz6DOz3U6bD8PIdDWS+SZib7A5GLIz5k8m8wRlYg3Gx2hY+0Jp3R+hBfjN8U" crossorigin="anonymous">
-        </script>
+
+        <!-- To automatically render math in text elements, include the auto-render extension: -->
+        <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/contrib/auto-render.min.js"
+            integrity="sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05" crossorigin="anonymous"
+            onload="renderMathInElement(document.body);"></script>
 
     </x-slot>
     <span id="math-equation">x + 12 = 3, \quad x = ?</span>
