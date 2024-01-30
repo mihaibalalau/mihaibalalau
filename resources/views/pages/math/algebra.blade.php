940707e6fd839be6dc963b51c727f4acaad6e641
Mihai Balalau <mihaibalalau@gmail.com>
2024-01-30 07:04:41 +0200

    added KaTeX

--- resources/views/pages/math/algebra.blade.php
+++ resources/views/pages/math/algebra.blade.php
@@ -1,59 +1,16 @@
 <x-layout>
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+    <x-slot:head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.13.0/katex.min.css"
+            integrity="sha384-R4558gJ3+9R5D1A+Hw3PDXCG+QG4Swhp0vYhIMpK8ENSOwEnClIDZZAM+SBh8Q1F" crossorigin="anonymous">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.13.0/katex.min.js"
+            integrity="sha384-fWURyB/6aPcIcKjRTPVFbETlFtRqNgp2krv4pJ5f6kPtJ8KDxK2e5IvRbUR7GxBI" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.13.0/contrib/auto-render.min.js"
+            integrity="sha384-Um5gpz6DOz3U6bD8PIdDWS+SZib7A5GLIz5k8m8wRlYg3Gx2hY+0Jp3R+hBfjN8U" crossorigin="anonymous">
+        </script>
 
-    <p>
-        When
-        <math xmlns="http://www.w3.org/1998/Math/MathML">
-            <mi>a</mi>
-            <mo>&#x2260;</mo>
-            <mn>0</mn>
-        </math>,
-        there are two solutions to
-        <math xmlns="http://www.w3.org/1998/Math/MathML">
-            <mi>a</mi>
-            <msup>
-                <mi>x</mi>
-                <mn>2</mn>
-            </msup>
-            <mo>+</mo>
-            <mi>b</mi>
-            <mi>x</mi>
-            <mo>+</mo>
-            <mi>c</mi>
-            <mo>=</mo>
-            <mn>0</mn>
-        </math>
-        and they are
-        <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
-            <mi>x</mi>
-            <mo>=</mo>
-            <mrow>
-                <mfrac>
-                    <mrow>
-                        <mo>&#x2212;</mo>
-                        <mi>b</mi>
-                        <mo>&#x00B1;</mo>
-                        <msqrt>
-                            <msup>
-                                <mi>b</mi>
-                                <mn>2</mn>
-                            </msup>
-                            <mo>&#x2212;</mo>
-                            <mn>4</mn>
-                            <mi>a</mi>
-                            <mi>c</mi>
-                        </msqrt>
-                    </mrow>
-                    <mrow>
-                        <mn>2</mn>
-                        <mi>a</mi>
-                    </mrow>
-                </mfrac>
-            </mrow>
-            <mtext>.</mtext>
-        </math>
-    </p>
+    </x-slot>
+    <span id="math-equation">x + 12 = 3, \quad x = ?</span>
 
     @vite(['resources/js/app.js', 'resources/css/app.css'])
 </x-layout>
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/math/algebra.css',
                 'resources/css/global.css',
                 'resources/css/app.css',
                 'resources/js/app.js',
